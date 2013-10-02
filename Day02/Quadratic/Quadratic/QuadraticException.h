e1aabdd1ebd7978c117ea7e85b194feef5c31562
svp <svp@svps-Mac.local>
2013-10-01 21:02:22 -0700

    Create new QuadraticException to handle delta less than zero case

---
+++ Day02/Quadratic/Quadratic/QuadraticException.h
@@ -0,0 +1,15 @@
+//
+//  QuadraticException.h
+//  Quadratic
+//
+//  Created by Dang Truong on 10/1/13.
+//  Copyright (c) 2013 eLeisure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuadraticException : NSException
+
+@property(readwrite)NSInteger *delta;
+
+@end
---
+++ Day02/Quadratic/Quadratic/QuadraticException.m
@@ -0,0 +1,13 @@
+//
+//  QuadraticException.m
+//  Quadratic
+//
+//  Created by Dang Truong on 10/1/13.
+//  Copyright (c) 2013 eLeisure. All rights reserved.
+//
+
+#import "QuadraticException.h"
+
+@implementation QuadraticException
+@synthesize delta;
+@end
