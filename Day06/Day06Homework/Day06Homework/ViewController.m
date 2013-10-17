70108818f58303c27a8e89f59a397b3f51d5e3fe
svp <svp@svps-Mac.local>
2013-10-17 09:36:07 -0700

    Update code zigzag using algorithm

--- Day06/Day06Homework/Day06Homework/ViewController.m
+++ Day06/Day06Homework/Day06Homework/ViewController.m
@@ -57,13 +57,52 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 - (IBAction)printZigzag:(id)sender {     
-    printf("*       *       *       *\n");
-    printf(" *     * *     * *     *\n");
-    printf("  *   *   *   *   *   *\n");
-    printf("   * *     * *     * *\n");
-    printf("    *       *       *\n");   
-    printf("\n");
-
+    // p: la dinh cua zigzag. co the thay doi p de thay doi kich thuoc cua zigza
+    // w: la do rong cua zigzag
+    int p = 9;
+    int w = 80;
+    
+    // luu zig zag vao mot mang 2 chieu
+    char zz[80][9];
+    // khoi tao gia tri cua zigzag voi toan ki tu rong
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < p; j++) {
+            zz[i][j]= ' ';
+        }
+    }
+    
+    // tao cac cham diem cho zigzag
+    int j = 0;
+    bool direction = true; // direction = true -> chieu cua zigzag di len, = false thi nguoc lai
+    
+    for (int i = 0; i < w; i++) {
+        zz[i][j] = '*';
+        if (j == (p-1)) {
+            direction = true;
+        }else if (j == 0)
+        {
+            direction = false;
+        }
+        
+        if (direction) {
+            j--;
+        }else
+        {
+            j++;
+        }            
+    }
+    
+    // in zigzag ra man hinh
+    for (j = 0; j < p; j++) {
+        for (int i = 0; i < w; i++) {
+            printf("%c", zz[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+- (IBAction)printCar:(id)sender {
+    // ve oto
     NSString *path = [[NSBundle mainBundle] pathForResource:@"car" ofType:@"txt"];
     NSString *car = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
     printf("%s", [car cStringUsingEncoding:NSUTF8StringEncoding]);
