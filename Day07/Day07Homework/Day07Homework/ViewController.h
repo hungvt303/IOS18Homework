4c36279bbaa0dc32c8033549ce18ee32b231c0bc
svp <svp@svps-Mac.local>
2013-10-22 10:06:37 -0700

    add day 07 homework again

---
+++ Day07/Day07Homework/Day07Homework/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Day07Homework
+//
+//  Created by iOS18 on 10/21/13.
+//  Copyright (c) 2013 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Day07/Day07Homework/Day07HomeworkTests/Day07HomeworkTests.m
@@ -0,0 +1,34 @@
+//
+//  Day07HomeworkTests.m
+//  Day07HomeworkTests
+//
+//  Created by iOS18 on 10/21/13.
+//  Copyright (c) 2013 None. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Day07HomeworkTests : XCTestCase
+
+@end
+
+@implementation Day07HomeworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
